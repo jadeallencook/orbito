a3db9d85762175dc2fc137705fc4b7aa606aefb5
Jade Allen Cook <jadeallencook@gmail.com>
2023-11-15 12:05:52 -0800

    added footer to app

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer>
+    <p>Games4Two | 2023</p>
+    <p>Developed by @jadeallencook</p>
+  </footer>
+);
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./style.css";
 import Navigation from "./components/Navigation";
+import Orbito from "./games/orbito";
+import Footer from "./components/Footer";
 
 const element = document.getElementById("root");
 const root = createRoot(element);
 
 const App = () => (
-  <div>
+  <>
     <Navigation />
-  </div>
+    <Orbito />
+    <Footer />
+  </>
 );
 
 root.render(<App />);
